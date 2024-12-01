6c6db1de9f8d228d32446f33485eb2ca27f72ce8
Christian <christian@Chimera-Server>
2024-12-01 07:16:31 -0500

    Continued table rendering iteration

--- src/Models/panes/Table.ts
+++ src/Models/panes/Table.ts
@@ -1,9 +1,5 @@
-import type {
-    ITableData,
-    ITable,
-    IMetadata,
-} from "$lib/data/pf2e/types/tables.ts";
-
+import type { ITableData, ITable, IMetadata } from "$lib/data/pf2e/types/tables.ts";
+import { error } from "@sveltejs/kit";
 
 export class TablePane {
     tables: ITable[];
@@ -12,32 +8,78 @@ export class TablePane {
 
     constructor(tableData: ITableData, domTarget: string) {
         if (!tableData.tables) {
-            throw new Error('Table is undefined')
+            throw new Error("Table is undefined");
         }
         this.tables = tableData.tables;
         this.metadata = tableData.metadata;
         this.target = document.getElementById(domTarget);
         if (!this.target) {
-            throw new Error(`Cannot find ${domTarget} in DOM`)
+            throw new Error(`Cannot find ${domTarget} in DOM`);
         }
         this.renderTable();
     }
 
     renderTable() {
-        console.log('render is starting')
+        console.log("render is starting");
+        let tableIndex = 0;
         this.tables.forEach((table) => {
-            const newTable = document.createElement('table');
-            this.target?.append(newTable)
             const columnTotal = table.columns.length;
-            const headerRow = document.createElement('tr');
-            newTable.append(headerRow)
+            let columnIndex = 0;
+            // Setting equal to 1, because we will first use it prior to the for loop when creating
+            // The header Row.
+            let rowIndex = 1;
+            tableIndex++;
+
+            // Create Table
+            const newTable = document.createElement("table");
+            newTable.setAttribute("id", `table-${tableIndex}`);
+            this.target?.append(newTable);
+
+            // Create Header Row
+            const headerRow = document.createElement("tr");
+            headerRow.setAttribute("id", `table-${tableIndex}-row-${rowIndex}`);
+            newTable.append(headerRow);
             // generate column headers
             table.columns.forEach((column) => {
-                const headerCell = document.createElement('th');
+                const headerCell = document.createElement("th");
                 headerCell.textContent = column.header;
                 headerRow.append(headerCell);
-            })
+            });
+            // create our <tr>'s -> id each <tr> -> re-enter each <tr> as we process each column
+            // Create all rows based on the largest column
+            let largestColumn: number = 0;
+            table.columns.forEach((column) => {
+                const rowReference = column.data.length;
+                if (!largestColumn) {
+                    largestColumn = rowReference;
+                }
+                if (rowReference > largestColumn) {
+                    largestColumn = rowReference;
+                }
+            });
+            if (largestColumn === 0) {
+                throw new Error("largest column is never set");
+            }
+            const maxColumnDataAmount = largestColumn;
+            while (largestColumn > 0) {
+                rowIndex++;
+                const createRow = document.createElement("tr");
+                createRow.setAttribute("id", `table-${tableIndex}-row-${rowIndex}`);
 
-        })
+                while (columnIndex < maxColumnDataAmount - 1) {
+                    table.columns.forEach((column) => {
+                        if (!column.data[columnIndex]) {
+                            const createCell = document.createElement("td");
+                            createCell.textContent = (column.data[columnIndex] as number).toString();
+                            createRow.append(createCell);
+                        }
+                    });
+                    columnIndex++;
+                }
+
+                newTable.append(createRow);
+                largestColumn--;
+            }
+        });
     }
 }
